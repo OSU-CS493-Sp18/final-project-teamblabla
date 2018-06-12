ff5d7360d7869f919f6ecb83373d296d83f96cdc
Omar <omariltaf@hotmail.co.uk>
2018-06-12 13:59:51 -0700

    Added the bcryptjs and jsonwebtoken dependencies.

--- api/index.js
+++ api/index.js
@@ -3,3 +3,4 @@ const router = module.exports = require('express').Router();
 const { router: rideRouter } = require('./rides');
 
 router.use('/rides', rideRouter);
+router.use('/users', require('./users').router);
